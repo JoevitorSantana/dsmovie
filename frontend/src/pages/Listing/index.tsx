b3cd3e73efd6290b1c19466756e73442eaac07a0
Joevitor Alves Santana <joevitoralvessantana@gmail.com>
2022-01-14 12:18:47 -0300

    First Request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,14 @@
+import axios from "axios";
 import { MovieCard } from "components/MovieCard";
 import { Pagination } from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 export function Listing(){
+
+    axios.get(`${BASE_URL}/movies/1`).then(response =>{
+        console.log(response.data);
+    })
+
     return(
         <>
             <Pagination />
